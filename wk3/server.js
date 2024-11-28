fe91ad0e91f6764f3b005e5f835046d6fd349d7f
AT <andrei.toma@ie.ase.ro>
2024-11-28 13:33:13 +0200

    .

--- wk3/server.js
+++ wk3/server.js
@@ -17,6 +17,7 @@ try {
 }
 
 const app = express()
+app.use(express.static('public'))
 app.use(express.json())
 
 app.get('/ping', (req, res) => {
@@ -42,32 +43,45 @@ app.post('/temperature-records', async (req, res, next) => {
     }
 })
 
-app.get('/temperature-records/:id', (req, res) => {
-    const record = records.find(e => e.id === parseInt(req.params.id))
-    if (record) {
-        res.status(200).json(record)
-    } else {
-        res.status(404).json({ message: 'not found' })
+app.get('/temperature-records/:id', async (req, res, next) => {
+    try {
+        const record = await TemperatureRecord.findByPk(req.params.id)
+        if (record) {
+            res.status(200).json(record)
+        } else {
+            res.status(404).json({ message: 'not found' })
+        }
+    } catch (error) {
+        next(error)
     }
 })
 
-app.put('/temperature-records/:id', (req, res) => {
-    const index = records.findIndex(e => e.id === parseInt(req.params.id))
-    if (index !== -1) {
-        records[index] = req.body
-        res.status(202).json({ message: 'accepted' })
-    } else {
-        res.status(404).json({ message: 'not found' })
+app.put('/temperature-records/:id', async (req, res, next) => {
+    try {
+        const record = await TemperatureRecord.findByPk(req.params.id)
+        if (record) {
+            record.level = req.body.level
+            await record.save()
+            res.status(202).json(record)
+        } else {
+            res.status(404).json({ message: 'not found' })
+        }
+    } catch (error) {
+        next(error)
     }
 })
 
-app.delete('/temperature-records/:id', (req, res) => {
-    const index = records.findIndex(e => e.id === parseInt(req.params.id))
-    if (index !== -1) {
-        records.splice(index, 1)
-        res.status(204).json({ message: 'deleted' })
-    } else {
-        res.status(404).json({ message: 'not found' })
+app.delete('/temperature-records/:id', async (req, res, next) => {
+    try {
+        const record = await TemperatureRecord.findByPk(req.params.id)
+        if (record) {
+            await record.destroy()
+            res.status(204).json({})
+        } else {
+            res.status(404).json({ message: 'not found' })
+        }
+    } catch (error) {
+        next(error)
     }
 })
 
