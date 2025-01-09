5fed036a6034aa3422f6829bb5221f5f9d417611
AT <andrei.toma@ie.ase.ro>
2025-01-09 12:31:51 +0200

    .

--- wk5/public/data-store.mjs
+++ wk5/public/data-store.mjs
@@ -1,33 +1,59 @@
 const SERVER = 'http://localhost:8080'
 
 class DataStore {
-    constructor () {
-        this.data = []
-    }
+	constructor () {
+		this.data = []
+	}
 
-    async getAll () {
-        const response = await fetch(`${SERVER}/temperature-records`)
-        if (!response.ok) {
-            throw response
-        } else {
-            this.data = await response.json()
-        }
-    }
+	async getAll () {
+		const response = await fetch(`${SERVER}/temperature-records`)
+		if (!response.ok) {
+			throw response
+		} else {
+			this.data = await response.json()
+		}
+	}
 
-    async addOne (item) {
-        const response = await fetch(`${SERVER}/temperature-records`, {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(item)
-        })
-        if (!response.ok) {
-            throw response
-        } else {
-            await this.getAll()
-        }
-    }
+	async addOne (item) {
+		const response = await fetch(`${SERVER}/temperature-records`, {
+			method: 'post',
+			headers: {
+					'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(item)
+		})
+		if (!response.ok) {
+			throw response
+		} else {
+			await this.getAll()
+		}
+	}
+
+	async saveOne (id, item) {
+		const response = await fetch(`${SERVER}/temperature-records/${id}`, {
+			method: 'put',
+			headers: {
+					'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(item)
+		})
+		if (!response.ok) {
+			throw response
+		} else {
+			await this.getAll()
+		}
+	}
+
+	async deleteOne (id) {
+		const response = await fetch(`${SERVER}/temperature-records/${id}`, {
+			method: 'delete'
+		})
+		if (!response.ok) {
+			throw response
+		} else {
+			await this.getAll()
+		}
+	}
 }
 
 const store = new DataStore()
